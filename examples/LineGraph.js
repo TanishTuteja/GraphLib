6dc33ef8ddb9f9705e3c78018172b6202c30758c
TanishTuteja <tanishtuteja@gmail.com>
2020-03-15 14:01:48 +0530

    Improved Library

--- examples/LineGraph.js
+++ examples/LineGraph.js
@@ -1,14 +1,14 @@
 var graph;
 
 function displayLineGraph() {
+  graph = new Graph(0.1, 1);
 
-    graph = new Graph(0.1, 1);
+  for (let i = 0; i < 100; i++) {
+    addRandomPoint();
+  }
+}
 
-    for (let i = 0; i < 100; i++) {
-
-        graph.addData(Math.random() * 100, Math.random() * 400);
-
-    }
-
-    graph.update();
+function addRandomPoint() {
+  graph.addData(Math.random() * 100, Math.random() * 400);
+  graph.update();
 }
--- src/Graph.js
+++ src/Graph.js
@@ -1,99 +1,106 @@
 class Graph {
+  constructor(xScale, yScale) {
+    //variable to hold canvas element
+    let myCanvas = document.createElement("canvas");
 
-    constructor(xScale, yScale) {
+    //height and width of canvas
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
 
-        //variable to hold canvas element
-        let myCanvas = document.createElement("canvas");
+    //margin of canvas
+    this.margin = 40;
 
-        //height and width of canvas
-        this.width = window.innerWidth;
-        this.height = window.innerHeight;
+    //array to hold data ------- data is an array of objects {x: ____ , y: ______}
+    this.data = [];
 
-        //margin of canvas
-        this.margin = 40;
+    //radius of points
+    this.pointRadius = 2;
 
-        //array to hold data ------- data is an array of objects {x: ____ , y: ______}
-        this.data = [];
+    //Scaling Factors ------ Datanumbers per pixel
+    this.xScale = xScale;
+    this.yScale = yScale;
 
-        //radius of points
-        this.pointRadius = 2;
+    //setting properties of canvas
+    myCanvas.setAttribute("class", "myCanvas");
+    myCanvas.setAttribute("width", this.width);
+    myCanvas.setAttribute("height", this.height);
 
-        //Scaling Factors ------ Datanumbers per pixel
-        this.xScale = xScale;
-        this.yScale = yScale;
+    //hiding scrollbars
+    document.body.style.margin = 0;
+    document.body.style.overflow = "hidden";
 
-        //setting properties of canvas
-        myCanvas.setAttribute("class", "myCanvas");
-        myCanvas.setAttribute("width", this.width);
-        myCanvas.setAttribute("height", this.height);
+    //adding canvas to body
+    document.body.appendChild(myCanvas);
 
-        //hiding scrollbars
-        document.body.style.margin = 0;
-        document.body.style.overflow = "hidden";
+    //get context of canvas to draw with
+    this.ctx = myCanvas.getContext("2d");
 
-        //adding canvas to body
-        document.body.appendChild(myCanvas);
+    this.drawStuff();
+  }
 
-        //get context of canvas to draw with
-        this.ctx = myCanvas.getContext('2d');
+  addData(xData, yData) {
+    let position = 0;
+    while (position < this.data.length && this.data[position].x < xData) {
+      position++;
+    }
 
-        //draw background
-        this.ctx.fillStyle = 'rgb(0, 0, 0)';
-        this.ctx.fillRect(0, 0, this.width, this.height);
+    this.data.splice(position, 0, {
+      x: xData,
+      y: yData
+    });
+  }
 
-        //draw axes
-        this.ctx.strokeStyle = "rgb(255,255,255)";
+  addDataArray(xData, yData) {
+    if (xData.length != yData.length) {
+      console.log(
+        "Error! xData and yData contain different number of elements"
+      );
+    } else {
+      for (let i = 0; i < xData.length; i++) {
+        this.addData(xData[i], yData[i]);
+      }
+    }
+  }
+
+  drawStuff() {
+    //draw background
+    this.ctx.fillStyle = "rgb(0, 0, 0)";
+    this.ctx.fillRect(0, 0, this.width, this.height);
+
+    //draw axes
+    this.ctx.strokeStyle = "rgb(255,255,255)";
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.margin, this.margin);
+    this.ctx.lineTo(this.margin, this.height - this.margin);
+    this.ctx.lineTo(this.width - this.margin, this.height - this.margin);
+    this.ctx.stroke();
+  }
+
+  update() {
+    this.drawStuff();
+
+    let pixelX = null;
+    let pixelY = null;
+
+    for (let i = 0; i < this.data.length; i++) {
+      const dataPoint = this.data[i];
+
+      let newPixelX = dataPoint.x / this.xScale + this.margin;
+      let newPixelY = this.height - (dataPoint.y / this.yScale + this.margin);
+
+      if (pixelX != null && pixelY != null) {
         this.ctx.beginPath();
-        this.ctx.moveTo(this.margin, this.margin);
-        this.ctx.lineTo(this.margin, this.height - this.margin);
-        this.ctx.lineTo(this.width - this.margin, this.height - this.margin);
+        this.ctx.moveTo(pixelX, pixelY);
+        this.ctx.lineTo(newPixelX, newPixelY);
         this.ctx.stroke();
+      }
 
+      pixelX = newPixelX;
+      pixelY = newPixelY;
+
+      this.ctx.beginPath();
+      this.ctx.arc(pixelX, pixelY, this.pointRadius, 0, 2 * Math.PI);
+      this.ctx.stroke();
     }
-
-    addData(xData, yData) {
-
-        this.data.push({
-            x: xData,
-            y: yData
-        });
-
-    }
-
-    update() {
-
-        let pixelX = null;
-        let pixelY = null;
-
-        this.data.sort(function (a, b) {
-            return (a.x - b.x);
-        });
-
-        for (let i = 0; i < this.data.length; i++) {
-
-            const dataPoint = this.data[i];
-
-            let newPixelX = dataPoint.x / this.xScale + this.margin;
-            let newPixelY = this.height - (dataPoint.y / this.yScale + this.margin);
-
-            if (pixelX != null && pixelY != null) {
-
-                this.ctx.beginPath();
-                this.ctx.moveTo(pixelX, pixelY);
-                this.ctx.lineTo(newPixelX, newPixelY);
-                this.ctx.stroke();
-
-            }
-
-            pixelX = newPixelX;
-            pixelY = newPixelY;
-
-            this.ctx.beginPath();
-            this.ctx.arc(pixelX, pixelY, this.pointRadius, 0, 2 * Math.PI);
-            this.ctx.stroke();
-
-        }
-
-    }
-
+  }
 }
